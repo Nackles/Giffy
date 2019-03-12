d28cae76b82226bcbb47da607a68cb9b400649ae
Nackles <45973617+Nackles@users.noreply.github.com>
2019-03-12 09:11:22 -0700

    safety

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+//var topics = [topic, topic]
+
+//each gif starts stopped, img.onclick != still
+
+//function createButtons {
+// function clear buttons
+//loop to go through and create&append each button
+//function createButtons() {
+//     $("<button>") jQuery to make the button
+//     .text(topic) instructions to change the text content
+//     .addClass("topicButton") for formatting
+//     .addClass("stopped")
+//      (OR) Gif Area.onclick(get img id and change class to moving)
+// }
+//              when clicked    this          sends a question to giphy
+// $(document).on("click", ".topicbuttons", giphyRing())
+
+//function giphyRing() {
+// }
+
+var topics = [ 'apex legends', 'league of legends', 'cats', 'salmon', 'visualizer', 'vr', 'augmented reality', 'magic the gathering', 'lost in space', 'dessert', 'bbq', 'roller coasters', 'cheese', 'potatoes', 'cheesy potatoes', 'swordfighting', 'cosmos' , 'snow', 'mountains', 'skiing', 'cool tricks', '90s', 'vaporwave', 'eyes', 'eyebrows'];
+
+
+    //onSUBMIT
+function createButtons() {
+    //clear button-collector div
+    //loop through topics topic.length times and make a button for each entry
+    //make a global variable storing current column
+    //make a loop that places gifs into the next column
+    //if statement within loop: if colnumber > 3, colnumber=0;
+}
+
+    //onBUTTONCLICK
+function gifRinger() {
+    //takes arguments for topic
+    //creates api url
+    //pings ajax
+    //then displays a 10-item selection in col1 OR carousel 1 (horizontal population might make more sense)
+    //4-tick loop that cycles through columns to place new gifs in
+    //each column could have a scrollbar
+    //each item DISPLAYS ITS RATING
+}
+
