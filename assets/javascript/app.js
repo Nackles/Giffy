6d767f675bc1ab4efaa0d8ebbe6bec55630bea8f
Nackles <45973617+Nackles@users.noreply.github.com>
2019-03-12 13:11:37 -0700

    safety push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
-//var topics = [topic, topic]
+var topics = ['apex legends', 'league of legends', 'cats', 'salmon', 'visualizer', 'vr', 'augmented reality', 'magic the gathering', 'lost in space', 'dessert', 'bbq', 'roller coasters', 'cheese', 'potatoes', 'cheesy potatoes', 'swordfighting', 'cosmos', 'snow', 'mountains', 'skiing', 'cool tricks', '90s', 'vaporwave', 'eyes', 'eyebrows'];
+
+//variable to designate which column to deposit gifs into 
+var colCurr = 0;
 
 //each gif starts stopped, img.onclick != still
 
@@ -15,22 +18,32 @@
 //              when clicked    this          sends a question to giphy
 // $(document).on("click", ".topicbuttons", giphyRing())
 
-//function giphyRing() {
-// }
-
-var topics = [ 'apex legends', 'league of legends', 'cats', 'salmon', 'visualizer', 'vr', 'augmented reality', 'magic the gathering', 'lost in space', 'dessert', 'bbq', 'roller coasters', 'cheese', 'potatoes', 'cheesy potatoes', 'swordfighting', 'cosmos' , 'snow', 'mountains', 'skiing', 'cool tricks', '90s', 'vaporwave', 'eyes', 'eyebrows'];
-
-
-    //onSUBMIT
+//onSUBMIT:
 function createButtons() {
+
     //clear button-collector div
+    $("buttons-collect").empty();
+
     //loop through topics topic.length times and make a button for each entry
-    //make a global variable storing current column
-    //make a loop that places gifs into the next column
-    //if statement within loop: if colnumber > 3, colnumber=0;
+    for (var i = 0; i < topics.length; i++) {
+        var buttonTag = $("<button>");
+        var buttonClickHandler = "#button-click";
+        var buttonValue = i;
+
+        $("buttons-collect").appendChild
+
+    }
+
 }
 
-    //onBUTTONCLICK
+//button creation
+//button <button> tag
+//id generate-gif
+//onclick event ajax query 
+
+
+
+//onBUTTONCLICK
 function gifRinger() {
     //takes arguments for topic
     //creates api url
@@ -41,3 +54,52 @@ function gifRinger() {
     //each item DISPLAYS ITS RATING
 }
 
+
+$("#button-click").on("click", function () {
+
+    var myAPI = "fpmcmDaPyMhRoYZdMK5FrTw9laKEKAWJ";
+    // api url
+    // api is suppose to return a random search using cat as our search term
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + myAPI + "&tag=" + searchFor;
+
+    //  jquery ajax call
+    // url => url to hit
+    // method => get request according to documentation
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        // .then is our Promise => it is triggered on any response
+        // pass in response as a parameter to capture the data obj returned
+        .then(function (response) {
+
+            // declare a variable and set it equal to the image uri using dot notation
+            var imageUrl = response.data.image_original_url;
+
+            // use jquery to dynamically create a html img element
+            var imageTag = $("<img>");
+
+            // reference catImage and use jquery attr method to add a value to the src attribute
+            imageTag.attr("src", imageUrl);
+            // reference catImage and use jquery attr method to add a value to the alt attribute
+            imageTag.attr("alt", "gifferondo");
+
+            // code for selecting the column
+            if (colCurr >= 2) {
+                colCurr = 0;
+            }
+            // HTMLtag variables
+            // var imageTag = "<img>";
+            // link from ajax
+            // var imageLink = "do i even need this";
+            // var thisRating = "rating from ajax call";
+
+            // use jquery to select the div with id=images and use jquery .prepend method to render the catImage div we just created
+            $("#images").prepend(imageTag);
+
+            //runs after placement so correct selection is ready next time
+            colCurr++;
+
+        });
+});
